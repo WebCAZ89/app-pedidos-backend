bece00815b23b5bc3fdae84bee519892a154f41f
WebCAZ89 <webcaz89@gmail.com>
2021-11-25 18:54:01 -0500

    Integración de servicios - caso correo electrónico

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,29 +1,28 @@
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
 import {Persona} from '../models';
 import {PersonaRepository} from '../repositories';
+import {AutenticacionService} from '../services/autenticacion.service';
+const fetch = require('node-fetch');
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
     public personaRepository : PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion: AutenticacionService
   ) {}
 
   @post('/personas')
@@ -44,7 +43,22 @@ export class PersonaController {
     })
     persona: Omit<Persona, 'id'>,
   ): Promise<Persona> {
-    return this.personaRepository.create(persona);
+
+    const clave = this.servicioAutenticacion.GenerarClave();
+    const claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    persona.clave = claveCifrada;
+    const p = await this.personaRepository.create(persona);
+
+    //Notificar al usuario
+    const destino = persona.correo;
+    const asunto = 'Registro en la plataforma';
+    const contenido = `Hola ${persona.nombres}, su nombre de usuario es: ${persona.correo} y su contraseña es: ${clave}`;
+    fetch(`http://127.0.0.1:5000/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+    .then((data: any) => {
+        console.log(data);
+      })
+    return p;
+
   }
 
   @get('/personas/count')
@@ -148,3 +162,5 @@ export class PersonaController {
     await this.personaRepository.deleteById(id);
   }
 }
+
+
--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,4 +1,6 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+const generador = require("password-generator");
+const cryptoJS = require("crypto-js");
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
@@ -7,4 +9,12 @@ export class AutenticacionService {
   /*
    * Add service methods here
    */
+
+  GenerarClave() {
+    const clave = generador(8, false)
+    return clave;
+  }
+  CifrarClave(clave: string){
+    const claveCifrada = cryptoJS.MD5(clave).toString();
+    return claveCifrada;}
 }
